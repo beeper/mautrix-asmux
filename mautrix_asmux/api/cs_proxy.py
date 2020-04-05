fa25feb35d7e778fab5a7c45a90e472fae364202
Tulir Asokan <tulir@maunium.net>
2020-04-05 16:25:27 +0300

    Fix auth token parsing

--- mautrix_asmux/api/cs_proxy.py
+++ mautrix_asmux/api/cs_proxy.py
@@ -152,7 +152,8 @@ class ClientProxy:
         try:
             auth = req.headers["Authorization"]
             assert auth and auth.startswith("Bearer ")
-            auth = auth.lstrip("Bearer ")
+            auth = auth[len("Bearer "):]
+            print(auth[:36], auth[37:])
             uuid = UUID(auth[:36])
             token = auth[37:]
         #except KeyError:
