9721a50895be5f6267dc279512b91f55a3f6628a
Nick Mills-Barrett <nick@beeper.com>
2022-04-20 11:01:20 +0100

    Execute websocket ping requests via Redis
    This means the asmux instance handling a ping request does not need
    to be connected to the bridge websocket in question. This works by:
    
    + Ping request comes in on instance A, publishes ping request
    + Instance A starts a loop waiting for the response on a Redis queue
    + Instance B sees the request and holds the websocket
    	- makes the ping request over the websocket
    	- pushes the result onto a Redis queue with 300s expiry
    + Instance A receives the response (or times out) and responds

--- mautrix_asmux/api/as_pinger.py
+++ mautrix_asmux/api/as_pinger.py
@@ -3,14 +3,19 @@
 from __future__ import annotations
 
 from typing import TYPE_CHECKING, cast
+from uuid import UUID
+import json
 import logging
 import time
+
+from aioredis import Redis
 
 from mautrix.types import UserID
 from mautrix.util.bridge_state import GlobalBridgeState
 from mautrix.util.logging import TraceLogger
 
 from ..database import AppService
+from ..redis import RedisPubSub
 from .as_util import make_ping_error
 
 if TYPE_CHECKING:
@@ -34,15 +39,77 @@ class AppServicePinger:
         server: "MuxServer",
         mxid_prefix: str,
         mxid_suffix: str,
+        redis: Redis,
+        redis_pubsub: RedisPubSub,
     ):
         self.server = server
         self.mxid_prefix = mxid_prefix
         self.mxid_suffix = mxid_suffix
+        self.redis = redis
+        self.redis_pubsub = redis_pubsub
+
+    async def setup(self):
+        self.log.info("Setting up Redis ping subscriptions")
+
+        await self.redis_pubsub.subscribe(
+            **{
+                PING_REQUEST_CHANNEL: self.handle_bridge_ping_request,
+            },
+        )
+
+    async def handle_bridge_ping_request(self, message: str) -> None:
+        """
+        Handles and executes websocket ping requests as requested via Redis.
+        """
+
+        az = await AppService.get(UUID(message))
+        if az and self.server.as_websocket.has_az_websocket(az):
+            self.log.debug(f"Handling ping request for AZ: {az.id}")
+            # pong = await self.server.as_websocket.ping(az)
+            pong = make_ping_error("io-timeout")
+            ping_request_queue = get_ping_request_queue(az)
+
+            async with self.redis.pipeline(transaction=True) as pipe:
+                pipe.rpush(ping_request_queue, json.dumps(pong.serialize()))
+                # Expire the queue after 5 minutes if nothing consumes it
+                pipe.expire(ping_request_queue, 300)
+                await pipe.execute()
+
+    async def request_bridge_ping(self, az: AppService) -> GlobalBridgeState:
+        """
+        This function requests a ping for a bridge websocket via Redis and returns
+        the response. This is implemented in a loop that retries up to 5 * 10s times
+        to receive the response before giving up. The `handle_bridge_ping_request`
+        method executes the actual ping requests this function sends.
+        """
+
+        ping_request_queue = get_ping_request_queue(az)
+        attempts = 0
+        max_attempts = 5
+
+        while True:
+            await self.redis.publish(PING_REQUEST_CHANNEL, str(az.id))
+            _, response = await self.redis.blpop(ping_request_queue, timeout=10)
+            if response:
+                break
+
+            attempts += 1
+            if attempts > max_attempts:
+                self.log.warning(
+                    f"Gave up waiting for ping response over Redis for {az.name} ({az.id})",
+                )
+                return make_ping_error("websocket-unknown-error")
+
+        data = json.loads(response)
+        # Workaround for: https://github.com/mautrix/python/pull/98
+        if "remote_states" not in data:
+            data["remoteState"] = None
+        return GlobalBridgeState.deserialize(data)
 
     async def ping(self, az: AppService) -> GlobalBridgeState:
         try:
             if not az.push:
-                pong = await self.server.as_websocket.ping(az)
+                pong = await self.request_bridge_ping(az)
             elif az.address:
                 pong = await self.server.as_http.ping(az)
             else:
