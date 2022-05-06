33221c55aa5d5dbd67d425277d9eff93a9fded3c
Nick Mills-Barrett <nick@beeper.com>
2022-05-06 21:38:29 +0100

    Fix missing async task

--- mautrix_asmux/api/as_websocket.py
+++ mautrix_asmux/api/as_websocket.py
@@ -391,7 +391,9 @@ class AppServiceWebsocketHandler:
                 await self._consume_queue_one(az, ws, queue)
         except Exception:
             self.log.exception("Fatal error in queue consumer")
-            ws.close(code=WSCloseCode.INTERNAL_ERROR, status="queue_consumer_failed")
+            asyncio.create_task(
+                ws.close(code=WSCloseCode.INTERNAL_ERROR, status="queue_consumer_failed")
+            )
         except asyncio.CancelledError as e:
             ws.log.debug(f"Queue consumer cancelled: {e}")
             raise
