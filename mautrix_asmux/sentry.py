a121ab64d926f0cf3314c41e5a695a32f9bca067
Nick Barrett <nick@beeper.com>
2022-07-13 15:18:03 +0200

    Add log during Sentry init

--- mautrix_asmux/sentry.py
+++ mautrix_asmux/sentry.py
@@ -1,8 +1,14 @@
+import logging
+
 from sentry_sdk.integrations.aiohttp import AioHttpIntegration
 import sentry_sdk
 
+log = logging.getLogger("mau.segment")
+
 
 def init(environment: str, dsn: str):
+    log.debug("Initializing Sentry for environment: %s", environment)
+
     sentry_sdk.init(
         environment=environment,
         dsn=dsn,
