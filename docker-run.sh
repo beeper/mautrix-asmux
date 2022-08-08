4e954f83fd28fe9add65e2692b64c12f80e88005
Nick Barrett <nick@beeper.com>
2022-08-08 09:17:02 +0100

    Restore ddtrace-run call
    Need this and the patching on server.

--- docker-run.sh
+++ docker-run.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 cd /opt/mautrix-asmux
 chown -R $UID:$GID /data /opt/mautrix-asmux
-exec su-exec $UID:$GID python3 -m mautrix_asmux -c /data/config.yaml
+if [ -z "$DD_AGENT_HOST" ]; then
+  exec su-exec $UID:$GID python3 -m mautrix_asmux -c /data/config.yaml
+else
+  exec su-exec $UID:$GID ddtrace-run python3 -m mautrix_asmux -c /data/config.yaml
+fi
